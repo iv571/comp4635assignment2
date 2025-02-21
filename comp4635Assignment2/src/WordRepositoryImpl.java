832366cf4c4a76490996c1c94e1bfe63ebb1f8ed
IV <IV@DESKTOP-VPTC5NS>
2025-02-20 19:49:21 -0700

    Edits to code

--- comp4635Assignment2/src/WordRepositoryImpl.java
+++ comp4635Assignment2/src/WordRepositoryImpl.java
@@ -119,7 +119,12 @@ public class WordRepositoryImpl extends UnicastRemoteObject implements WordRepos
     public static void main(String[] args) {
         try {
             // Create or get the registry on port 1099.
-        	java.rmi.registry.LocateRegistry.createRegistry(1100);
+        	try {
+        	    java.rmi.registry.LocateRegistry.getRegistry(1100).list();
+        	} catch (Exception e) {
+        	    // If the registry doesn't exist, create it.
+        	    java.rmi.registry.LocateRegistry.createRegistry(1100);
+        	}
             // Create an instance of the repository implementation.
             WordRepositoryImpl wordServer = new WordRepositoryImpl();
             // Bind the repository instance in the registry with the name "WordRepositoryServer".
