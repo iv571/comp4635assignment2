a0593f5aa5359fb592bd85ad2c5f2ff678187b38
Khanh Le <lekhanhsone@gmail.com>
2025-03-23 13:38:27 -0600

    Revert the change

--- comp4635Assignment2/src/CrissCrossPuzzleServer.java
+++ comp4635Assignment2/src/CrissCrossPuzzleServer.java
@@ -1,29 +1,48 @@
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
+import java.util.Map;
 
 public interface CrissCrossPuzzleServer extends Remote {
 
-    // Single-player methods with deduplication parameters (username and sequence number)
-    public String startGame(String player, int number_of_words, int failed_attempt_factor, int seq) throws RemoteException;
-    public String guessLetter(String player, char letter, int seq) throws RemoteException;
-    public String guessWord(String player, String word, int seq) throws RemoteException;
-    public String endGame(String player, int seq) throws RemoteException;
-    public String restartGame(String player, int seq) throws RemoteException;
+	public String startGame(
+			String player,
+			int number_of_words,
+			int failed_attempt_factor) throws RemoteException;
 
-    // Word repository operations (if applicable, they now take username and sequence number)
-    public boolean addWord(String username, String word, int seq) throws RemoteException;
-    public boolean removeWord(String username, String word, int seq) throws RemoteException;
-    public boolean checkWord(String username, String word, int seq) throws RemoteException;
+	public String guessLetter(String player, char letter) throws RemoteException;
 
-    // Multiplayer methods with deduplication parameters
-    public String startMultiGame(String username, int numPlayers, int level, int seq) throws RemoteException, RejectedException;
-    public String joinMultiGame(String player, int gameId, ClientCallback callback, int seq) throws RemoteException, RejectedException;
-    public String startGameRoom(String hostName, int gameId, int seq) throws RemoteException;
-    public String setActivePlayer(String player, int gameId, int seq) throws RemoteException;
-    public String leaveRoom(String player, int gameId, int seq) throws RemoteException;
-    public boolean isActiveRoom(String username, int gameId, int seq) throws RemoteException;
-    public boolean isGameRun(String username, int gameId, int seq) throws RemoteException;
-    public String runGame(String player, int roomId, WordRepositoryServer wordServer, int seq) throws RemoteException;
-    public String showActiveGameRooms(String username, int seq) throws RemoteException;
-    public boolean isValidRoomID(String username, int roomID, int seq) throws RemoteException;
+	public String guessWord(String player, String word) throws RemoteException;
+
+	public String endGame(String player) throws RemoteException;
+
+	public String restartGame(String player) throws RemoteException;
+
+	public boolean addWord(String word) throws RemoteException;
+
+	public boolean removeWord(String word) throws RemoteException;
+
+	public boolean checkWord(String word) throws RemoteException;
+
+	public String startMultiGame(String username, int numPlayers, int level) throws RemoteException, RejectedException;
+
+	public String joinMultiGame(String player, int gameId, ClientCallback callback)
+			throws RemoteException, RejectedException;
+
+	public String startGameRoom(String hostName, int gameId) throws RemoteException;
+
+	public String setActivePlayer(String player, int gameId) throws RemoteException;
+
+	public String leaveRoom(String player, int gameId) throws RemoteException;
+
+	public boolean isActiveRoom(int gameId) throws RemoteException;
+
+	public boolean isGameRun(int gameId) throws RemoteException;
+
+	public String runGame(String player, int roomId, WordRepositoryServer wordServer) throws RemoteException;
+
+	public String showActiveGameRooms() throws RemoteException;
+
+	public boolean isValidRoomID(int roomID) throws RemoteException;
 }
